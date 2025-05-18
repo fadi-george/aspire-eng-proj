65adc6fb5a82cfaca5b783fdc53fdab9dfa8604b
Fadi George <fadii925@gmail.com>
2025-05-17 18:38:35 -0700

    add search repository query

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,51 @@
 import { createSchema, createYoga } from "graphql-yoga";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit();
+
+interface GitHubRepository {
+  name: string;
+  description: string | null;
+  html_url: string;
+  stargazers_count: number;
+  language: string | null;
+}
 
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: /* GraphQL */ `
+      type Repository {
+        name: String!
+        description: String
+        url: String!
+        stars: Int!
+        language: String
+      }
+
       type Query {
         hello: String
+        searchRepositories(query: String!, limit: Int = 10): [Repository!]!
       }
     `,
     resolvers: {
       Query: {
         hello: () => "Hello from Yoga!",
+        searchRepositories: async (_, { query, limit }) => {
+          const response = await octokit.rest.search.repos({
+            q: query,
+            per_page: limit,
+            sort: "stars",
+            order: "desc",
+          });
+
+          return response.data.items.map((repo: GitHubRepository) => ({
+            name: repo.name,
+            description: repo.description,
+            url: repo.html_url,
+            stars: repo.stargazers_count,
+            language: repo.language,
+          }));
+        },
       },
     },
   }),
