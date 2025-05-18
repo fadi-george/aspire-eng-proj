c7c30a034d02585e713831a99393eaceab94d9e4
Fadi George <fadii925@gmail.com>
2025-05-18 00:55:08 -0700

    having floating list for autocomplete

--- app/src/routes/-home/search.tsx
+++ app/src/routes/-home/search.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { toast } from "sonner";
 import {
   Command,
@@ -22,6 +22,7 @@ export const Search = () => {
   const [search, setSearch] = useState("");
   const debouncedSearch = useDebounce(search, 500);
   const queryClient = useQueryClient();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   // Example of using the search repositories query
   const { data: repositories = [], isLoading: isLoadingRepos } = useQuery({
@@ -52,45 +53,53 @@ export const Search = () => {
   };
 
   return (
-    <Command className="rounded-lg border shadow-md md:min-w-[450px]">
-      <CommandInput
-        placeholder="Search repositories"
-        className="h-9"
-        value={search}
-        onValueChange={(value) => setSearch(value)}
-      />
-      {debouncedSearch.length > 0 && (
-        <CommandList>
-          {!isLoadingRepos && repositories.length === 0 && (
-            <CommandEmpty>No repositories found.</CommandEmpty>
-          )}
-          {isLoadingRepos && <CommandEmpty>Loading...</CommandEmpty>}
-          <CommandGroup>
-            {repositories.map((repository) => (
-              <CommandItem
-                className="cursor-pointer"
-                key={repository.url}
-                value={repository.url}
-                onSelect={() => handleTrackRepository(repository)}
-              >
-                <div className="flex items-center gap-2 justify-between w-full">
-                  <div className="flex flex-col overflow-hidden flex-1 ">
-                    <p className="text-sm font-medium">{repository.name}</p>
-                    <p className="text-sm text-muted-foreground overflow-ellipsis whitespace-nowrap block overflow-hidden">
-                      {repository.description}
-                    </p>
+    <div className="relative w-full">
+      <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+        <CommandInput
+          placeholder="Search repositories"
+          className="h-9"
+          value={search}
+          onValueChange={(value) => setSearch(value)}
+          ref={inputRef}
+          onKeyDown={(e) => {
+            if (e.key === "Escape") {
+              setSearch("");
+            }
+          }}
+        />
+        {debouncedSearch.length > 0 && (
+          <CommandList className="absolute right-0 left-0 top-12 bg-white shadow-md border rounded-lg">
+            {!isLoadingRepos && repositories.length === 0 && (
+              <CommandEmpty>No repositories found.</CommandEmpty>
+            )}
+            {isLoadingRepos && <CommandEmpty>Loading...</CommandEmpty>}
+            <CommandGroup>
+              {repositories.map((repository) => (
+                <CommandItem
+                  className="cursor-pointer"
+                  key={repository.url}
+                  value={repository.url}
+                  onSelect={() => handleTrackRepository(repository)}
+                >
+                  <div className="flex items-center gap-2 justify-between w-full">
+                    <div className="flex flex-col overflow-hidden flex-1 ">
+                      <p className="text-sm font-medium">{repository.name}</p>
+                      <p className="text-sm text-muted-foreground overflow-ellipsis whitespace-nowrap block overflow-hidden">
+                        {repository.description}
+                      </p>
+                    </div>
+                    <div className="ml-auto flex items-center gap-2 text-nowrap overflow-hidden">
+                      <p className="text-sm text-muted-foreground">
+                        {formatNumber(repository.stars, 1)} ★
+                      </p>
+                    </div>
                   </div>
-                  <div className="ml-auto flex items-center gap-2 text-nowrap overflow-hidden">
-                    <p className="text-sm text-muted-foreground">
-                      {formatNumber(repository.stars, 1)} ★
-                    </p>
-                  </div>
-                </div>
-              </CommandItem>
-            ))}
-          </CommandGroup>{" "}
-        </CommandList>
-      )}
-    </Command>
+                </CommandItem>
+              ))}
+            </CommandGroup>{" "}
+          </CommandList>
+        )}
+      </Command>
+    </div>
   );
 };
