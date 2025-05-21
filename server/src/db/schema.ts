663a16a7f3ba9ea1179e387253d5452ceba9ac48
Fadi George <fadii925@gmail.com>
2025-05-21 03:17:48 -0700

    add repo and tracked repo schemas

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,10 +1,12 @@
 import {
+  bigint,
   index,
   integer,
   pgTable,
   serial,
   text,
   timestamp,
+  unique,
   varchar,
 } from "drizzle-orm/pg-core";
 
@@ -19,7 +21,40 @@ export const users = pgTable(
     createdAt: timestamp("created_at").notNull().defaultNow(),
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
   },
-  (table) => ({
-    githubIdIdx: index("github_id_idx").on(table.githubId),
-  })
+  (table) => [index("github_id_idx").on(table.githubId)]
+);
+
+export const repositories = pgTable(
+  "repositories",
+  {
+    id: serial("id").primaryKey(),
+    repoId: bigint("repo_id", { mode: "number" }).notNull().unique(),
+    name: text("name").notNull(),
+    owner: text("owner").notNull(),
+    description: text("description"),
+    publishedAt: timestamp("published_at"),
+    tagName: text("tag_name"),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+  },
+  (table) => [unique("unique_repo").on(table.owner, table.name)]
+);
+
+export const trackedRepositories = pgTable(
+  "tracked_repositories",
+  {
+    id: serial("id").primaryKey(),
+    userId: integer("user_id")
+      .notNull()
+      .references(() => users.id, {
+        onDelete: "cascade",
+      }),
+    repoId: bigint("repo_id", { mode: "number" })
+      .notNull()
+      .references(() => repositories.repoId),
+    lastSeenTag: text("last_seen_tag"),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+  },
+  (table) => [unique("unique_user_repo").on(table.userId, table.repoId)]
 );
